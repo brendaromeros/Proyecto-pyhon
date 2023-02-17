e87e7cc363470cf48b4f56a2f5dd3d467da4668f
Brenda Romero <brenda.romero@gmail.com>
2023-02-16 23:30:02 -0600

    Creación del repositorio

---
+++ Cámara_inedito_web/Cámara_inedito_web/wsgi.py
@@ -0,0 +1,17 @@
+"""
+WSGI config for Cámara_inedito_web project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+from django.conf import settingso
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Cámara_inedito_web.settings")
+
+application = get_wsgi_application()
