c459f06d54f8303e598066ea82d69836e307f541
Brenda Romero <brenda.romero@gmail.com>
2023-02-18 23:10:34 -0600

    "Añadiendo 4 modelos nuevos"

--- Cámara_inedito_web/homepage/views.py
+++ Cámara_inedito_web/homepage/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
+from .models import Phrases, Lyric, Stories, Poetry
+from .models import Author, Genre
 
 
 # Create your views here.
+
 def home(request):
-    return render(request, 'homepage/home.html')
+    """
+    Función vista para la página inicio del sitio.
+    """
+    # Genera contadores de algunos de los objetos principales
+    num_stories=Stories.objects.all().count()
+    num_authors=Author.objects.count()
+    num_lyricss = Lyrics.objects.all().count()
+    num_phrasess = Phrases.objects.all().count()
+    num_poetrys = Poetry.objects.all().count()
+    
+    # Renderiza la plantilla HTML index.html con los datos en la variable contexto
+    return render(
+        request,
+         'homepage/home.html',
+        context={
+            'num_stories':num_stories,
+            'num_authors':num_authors,
+            'num_lyricss':num_lyricss,
+            'num_phrasess':num_phrasess,
+            'num_poetrys':num_poetrys}
+
+    )
+
