bbf07ee5c99db5e08e86eb759987c5a9d22fc227
Brenda Romero <brenda.romero@gmail.com>
2023-02-19 18:50:48 -0600

    "Vistas y modelos"

--- Cámara_inedito_web/homepage/admin.py
+++ Cámara_inedito_web/homepage/admin.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from .models import Phrases, Lyric, Stories, Poetry
-from .models import Author, Genre
+from .models import Author, Genre, Other, Media
 
 admin.site.register(Phrases)
 admin.site.register(Author)
@@ -9,3 +9,5 @@ admin.site.register(Genre)
 admin.site.register(Stories)
 admin.site.register(Lyric)
 admin.site.register(Poetry)
+admin.site.register(Other)
+admin.site.register(Media)
--- Cámara_inedito_web/homepage/models.py
+++ Cámara_inedito_web/homepage/models.py
@@ -52,20 +52,26 @@ class Stories(models.Model):
 
     content = models.TextField(
         max_length=10000000000,
+				blank=True,
         help_text="Ingrese el contenido del relato")
 
     genre = models.ManyToManyField(
         Genre,
+				blank=True,
         help_text="Seleccione un género para este relato")
 
     score = models.PositiveSmallIntegerField(
-        help_text="Calificación del relato"
+				blank=True,
+        help_text="Calificación del relato",
+				default=0
     )
 
     front = models.ImageField(
+				blank=True,
         help_text="Ingrese una imagen de portada para el relato")
 
     files = models.FileField(
+				blank=True,
         help_text="Suba el archivo en formato pdf"
     )
 
@@ -99,24 +105,31 @@ class Poetry(models.Model):
 
     content = models.TextField(
         max_length=10000000000,
+				blank=True,
         help_text="Ingrese el contenido de la poesía")
 
     genre = models.ManyToManyField(
         Genre,
+				blank=True,
         help_text="Seleccione un género para esta poesía")
 
     score = models.PositiveSmallIntegerField(
+				blank=True,
+				default=0,
         help_text="Calificación de la poesía"
     )
 
     front = models.ImageField(
+				blank=True,
         help_text="Ingrese una imagen de portada para la poesía")
 
     files = models.FileField(
+				blank=True,
         help_text="Suba el archivo en formato pdf"
     )
 
     last_modified = models.DateField(
+				blank=True,
         auto_now=True,
         help_text="última modificación")
 
@@ -146,20 +159,26 @@ class Phrases(models.Model):
 
     content = models.TextField(
         max_length=10000000000,
+				blank=True,
         help_text="Ingrese el contenido de la frase")
 
     genre = models.ManyToManyField(
         Genre,
+				blank=True,
         help_text="Seleccione un género para esta frae")
 
     score = models.PositiveSmallIntegerField(
-        help_text="Calificación de la frase"
+        help_text="Calificación de la frase",
+				blank=True,
+				default=0
     )
 
     front = models.ImageField(
+				blank=True,
         help_text="Ingrese una imagen de portada para la frase")
 
     files = models.FileField(
+				blank=True,
         help_text="Suba el archivo en formato pdf"
     )
 
@@ -193,20 +212,26 @@ class Lyric(models.Model):
 
     content = models.TextField(
         max_length=10000000000,
-        help_text="Ingrese el contenido de la lírica")
+        help_text="Ingrese el contenido de la lírica",
+				blank=True)
 
     genre = models.ManyToManyField(
         Genre,
+				blank=True,
         help_text="Seleccione un género para esta lírica")
 
     score = models.PositiveSmallIntegerField(
+				blank=True,
+				default=0,
         help_text="Calificación de la lírica"
     )
 
     front = models.ImageField(
+				blank=True,
         help_text="Ingrese una imagen de portada para la lírica")
 
     files = models.FileField(
+				blank=True,
         help_text="Suba el archivo en formato pdf"
     )
 
@@ -225,3 +250,114 @@ class Lyric(models.Model):
         Devuelve el URL a una instancia particular de Lyric
         """
         return reverse('lyric-detail', args=[str(self.id)])
+
+
+class Other(models.Model):
+    """
+    Modelo que representa un escrito
+    """
+
+    name = models.CharField(max_length=200)
+
+    playlist = models.CharField(max_length=200)
+
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.SET_NULL, null=True)
+
+    content = models.TextField(
+        max_length=10000000000,
+				blank=True,
+        help_text="Ingrese el contenido del escrito")
+
+    genre = models.ManyToManyField(
+        Genre,
+				blank=True,
+        help_text="Seleccione un género para este escrito")
+
+    score = models.PositiveSmallIntegerField(
+				blank=True,
+				default=0,
+        help_text="Calificación del escrito"
+    )
+
+    front = models.ImageField(
+				blank=True,
+        help_text="Ingrese una imagen de portada para el escrito")
+
+    files = models.FileField(
+				blank=True,
+        help_text="Suba el archivo en formato pdf"
+    )
+
+    last_modified = models.DateField(
+        auto_now=True,
+        help_text="última modificación")
+
+    def __str__(self):
+        """
+        String que representa al objeto Other
+        """
+        return self.name
+
+    def get_absolute_url(self):
+        """
+        Devuelve el URL a una instancia particular de Other
+        """
+        return reverse('other-detail', args=[str(self.id)])
+
+
+class Media(models.Model):
+    """
+    Modelo que representa contenido multimedia
+    """
+
+    name = models.CharField(max_length=200)
+
+
+    author = models.ForeignKey(
+        Author,
+        on_delete=models.SET_NULL, null=True)
+
+    content = models.TextField(
+        max_length=10000000000,
+				blank=True,
+        help_text="Ingrese el contenido del archivo")
+
+    genre = models.ManyToManyField(
+        Genre,
+				blank=True,
+        help_text="Seleccione un género para este archivo")
+
+    score = models.PositiveSmallIntegerField(
+				blank=True,
+				default=0,
+        help_text="Calificación del archivo"
+    )
+
+    front = models.ImageField(
+				blank=True,
+        help_text="Ingrese una imagen de portada para el archivo")
+
+    files = models.FileField(
+				blank=True,
+        help_text="Suba el archivo en formato pdf"
+    )
+
+    last_modified = models.DateField(
+        auto_now=True,
+        help_text="última modificación")
+
+    def __str__(self):
+        """
+        String que representa al objeto Media
+        """
+        return self.name
+
+    def get_absolute_url(self):
+        """
+        Devuelve el URL a una instancia particular de Media
+        """
+        return reverse('media-detail', args=[str(self.id)])
+
+
