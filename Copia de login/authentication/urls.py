d07189a25e1ec13f5d48c0dfd0011786a6772c46
Nayeli Romero <124866699+brendaromeros@users.noreply.github.com>
2023-02-20 11:13:05 +0000

    Cambios a lista

--- Copia de login/authentication/urls.py
+++ Copia de login/authentication/urls.py
@@ -1,17 +1,18 @@
 from django.contrib import admin
 from django.urls import path, include
+from homepage import views as menu
 from . import views
+
 
 urlpatterns = [
    path('', views.home, name="home"),
    path('signup', views.signup, name='signup'),
    path('signin', views.signin, name='signin'),
    path('signout', views.signout, name='signout'),
-   path('relatos', views.relatos, name='relatos'),
-   path('liricas', views.liricas, name='liricas'),
-   path('escritos', views.escritos, name='escritos'),
+   path('relatos', menu.StoriesListView, name='relatos'),
+   path('liricas', views.LyricListView, name='liricas'),
+   path('poesia', views.PoetryListView, name='poesia'),
    path('principal', views.principal, name='principal'),
    path('index', views.index, name='index')
-
 
 ]
