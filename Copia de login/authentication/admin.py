351f2885b18bae4c625507aa0b2466891274994c
Nayeli Romero <124866699+brendaromeros@users.noreply.github.com>
2023-02-20 09:27:41 +0000

    Cambiar ubicación de homepage

--- Copia de login/authentication/admin.py
+++ Copia de login/authentication/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Phrases, Lyric, Stories, Poetry
+from .models import Author, Genre, Other, Media
+
+admin.site.register(Phrases)
+admin.site.register(Author)
+admin.site.register(Genre)
+admin.site.register(Stories)
+admin.site.register(Lyric)
+admin.site.register(Poetry)
+admin.site.register(Other)
+admin.site.register(Media)
